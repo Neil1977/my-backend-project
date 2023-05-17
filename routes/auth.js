a9e3e4565941e5230894994b9be4fb56f8247fa1
Neil1977 <Neil_lunn1977@hotmail.com>
2023-05-17 15:47:36 +0100

    updated auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,5 @@
-// Import the Express framework
+// Import dependencies
 const express = require("express");
-
-// Import the jsonwebtoken library
 const jwt = require("jsonwebtoken");
 
 // Create a new instance of the Express router
@@ -26,18 +24,24 @@ router.get("/callback", (req, res) => {
     afterCallback: "/dashboard", // Redirecting to the specified page after successful callback
     failureRedirect: "/login", // Redirecting to the specified page in case of authentication failure
     successCallback: async (req, res) => {
-      // Generate a JWT token using Auth0
-      const token = jwt.sign(
-        { sub: req.oidc.user.sub }, // Include the user's sub (subject) identifier in the token
-        process.env.JWT_SECRET, // Use the JWT_SECRET from the environment variables
-        { expiresIn: "1h" } // Set the token expiration time
-      );
+      try {
+        // Generate a JWT token using Auth0
+        const token = jwt.sign(
+          { sub: req.oidc.user.sub }, // Include the user's sub (subject) identifier in the token
+          "fcc5709a62306ca14fac1dfc7ca83884ec29317febd2a427810415a9235e60b8",
+          { expiresIn: "1h" } // Set the token expiration time
+        );
 
-      // Store the JWT token in a secure HTTP-only cookie
-      res.cookie("token", token, { httpOnly: true });
+        // Store the JWT token in a secure HTTP-only cookie
+        res.cookie("token", token, { httpOnly: true });
 
-      // Redirect to the specified page after successful callback
-      res.redirect("/dashboard");
+        // Redirect to the specified page after successful callback
+        res.redirect("/dashboard");
+      } catch (error) {
+        console.error("Error generating JWT token:", error);
+        // Handle the error
+        res.status(500).send("Error generating JWT token");
+      }
     },
   });
 });
