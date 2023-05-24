f64f0f51a3195cfa00780123dcd8f7d8e29c183d
Neil1977 <Neil_lunn1977@hotmail.com>
2023-05-24 12:28:32 +0100

    updated routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,44 @@ const router = express.Router();
 const middleware = require("../path/to/middleware.js");
 
 // Define your routes here
-router.get("/dashboard", middleware, (req, res) => {
-  // Route logic goes here
+
+// Route to create a new post
+router.post("/posts/create", middleware, (req, res) => {
+  // Extract the necessary data from the request body
+  const { title, content } = req.body;
+
+  // Perform any necessary validation on the data
+
+  // If the data is valid, create a new post
+  const newPost = {
+    title,
+    content,
+    // Include any additional fields or data
+  };
+
+  // Save the new post to the database or perform any other necessary actions
+
+  // Return a success response
+  res.status(200).json({ message: "Post created successfully", post: newPost });
+});
+
+// Route to retrieve a specific post by its ID
+router.get("/posts/:id", middleware, (req, res) => {
+  // Extract the post ID from the request parameters
+  const postId = req.params.id;
+
+  // Retrieve the post from the database based on the ID
+  // Perform any necessary error handling or validation
+
+  // If the post is found, return it in the response
+  const post = {
+    id: postId,
+    title: "Example Post",
+    content: "This is the content of the post.",
+    // Include any additional fields or data
+  };
+
+  res.status(200).json(post);
 });
 
 // Export the router
