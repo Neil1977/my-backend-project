4524431d845a253644fbb35ad274dba4cfa95cfe
Neil1977 <Neil_lunn1977@hotmail.com>
2023-06-02 15:48:52 +0100

    updated

--- routes/server/tests/api.test.js
+++ routes/server/tests/api.test.js
@@ -1,31 +1,31 @@
 const request = require('supertest');
 const app = require('../server');
 
-// Increase the timeout value for the test cases
-jest.setTimeout(10000);
-
 describe('Authentication', () => {
+  afterAll((done) => {
+    app.close(done);
+  });
+
   it('should authenticate a user and return a JWT token', (done) => {
     const user = {
       email: 'test@example.com',
       password: 'test123',
     };
 
-    // Send a POST request to the '/login' endpoint with the user credentials
     request(app)
       .post('/login')
       .send(user)
-      .expect(200) // Expect a 200 response status code
-      .expect('Content-Type', /json/) // Expect the response to have JSON content type
+      .expect(200)
+      .expect('Content-Type', /json/)
       .end((err, response) => {
-        if (err) return done(err);
-        // Expect the response body to have a 'token' property
-        expect(response.body).toHaveProperty('token');
-        done();
+        if (err) {
+          done(err);
+        } else {
+          expect(response.body).toHaveProperty('token');
+          done();
+        }
       });
   });
-
-  // Add more test cases for authentication if needed
 });
 
 describe('Protected Routes', () => {
@@ -37,32 +37,35 @@ describe('Protected Routes', () => {
       password: 'test123',
     };
 
-    // Authenticate a user and obtain the JWT token
     request(app)
       .post('/login')
       .send(user)
       .end((err, response) => {
-        if (err) return done(err);
-        authToken = response.body.token;
-        done();
+        if (err) {
+          done(err);
+        } else {
+          authToken = response.body.token;
+          done();
+        }
       });
+  });
+
+  afterAll((done) => {
+    app.close(done);
   });
 
   it('should get a protected resource when authorized', (done) => {
-    // Send a GET request to the '/protected' endpoint with the JWT token in the 'Authorization' header
     request(app)
       .get('/protected')
       .set('Authorization', `Bearer ${authToken}`)
-      .expect(200) // Expect a 200 response status code
-      .expect('Content-Type', /json/) // Expect the response to have JSON content type
-      .end((err, response) => {
-        if (err) return done(err);
-        done();
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err) => {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
       });
   });
-
-  // Add more test cases for authorized and unauthorized requests
 });
-
-module.exports = app;
-
